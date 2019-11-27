4a2240169fa274b9fa0a667981a5edea2b169ffc
xiaoxw <15949@millionser.com>
2019-11-27 09:16:57 +0800

    118.杨辉三角

---
+++ problems/PascalsTriangle.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace problems;
+
+/**
+ * 杨辉三角
+ * https://leetcode-cn.com/problems/pascals-triangle/
+ * Class YanghuiTriangle
+ * @package problems
+ */
+class PascalsTriangle
+{
+    function solution($numRows)
+    {
+        if ($numRows == 0) {
+            return [];
+        }
+
+        $triangle = [[1]];
+
+        for ($i=1; $i<$numRows; $i++) {
+            for ($j=0; $j<($i+1); $j++) {
+                isset($triangle[$i-1][$j-1]) ? $num1 = $triangle[$i-1][$j-1] : $num1 = 0;
+                isset($triangle[$i-1][$j]) ? $num2 = $triangle[$i-1][$j] : $num2 = 0;
+                $triangle[$i][$j] = $num1+$num2;
+            }
+        }
+
+        return $triangle;
+    }
+}
+
+$re = new PascalsTriangle();
+$tests = [
+    5,
+];
+
+$t = [
+    [1],
+    [1,1],
+    [1,2,1],        //  2 1   0 1     A[i-1][j-1] + A[i-1][j]
+    [1,3,3, 1],     //  3 1   0 1  3 2 1 2
+    [1,4,6, 4, 1],
+    [1,5,10,10,5, 1]
+];
+foreach ($tests as $test) {
+    $res = $re->solution($test);
+    var_dump($res);
+}
